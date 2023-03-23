41105581fb11df5e0cb2b87a32bb92a80155558e
Romain Trochu <romain.trochu@gmail.com>
2023-03-23 22:35:24 +0100

    add avatar to fixtures, front

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -26,35 +26,35 @@ class AppFixtures extends Fixture
         $faker = Factory::create('fr_FR');
         $gender = [null, 'male', 'female'];
         
-        // for($i = 0; $i <5; $i++){
-        //     $user = new User();
-        //     $user->setEmail($faker->word().'@mail.com');
-        //     $user->setPassword($faker->word()); //@todo rmettre le hasher $this->hasher->hashPassword($user, $faker->word())
-        //     $user->setRoles(['ROLE_USER']);
-        //     $user->setFirstname($faker->firstName($gender[array_rand($gender)]));
-        //     $user->setUsername($faker->lastName());
-        //     $user->setAvatar('image.png');
-        //     $user->setDateOfBirthAt(\DateTimeImmutable::createFromMutable($faker->dateTimeInInterval("-30 years", "+10 years")));
-        //     $user->setBiography($faker->text());
+        for($i = 0; $i <5; $i++){
+            $user = new User();
+            $user->setEmail($faker->word().'@mail.com');
+            $user->setPassword($this->hasher->hashPassword($user, "password")); //@todo rmettre le hasher $this->hasher->hashPassword($user, $faker->word())
+            $user->setRoles(['ROLE_USER']);
+            $user->setFirstname($faker->firstName($gender[array_rand($gender)]));
+            $user->setUsername($faker->lastName());
+            $user->setAvatar("https://randomuser.me/api/portraits/men/".rand(1,99).".jpg");
+            $user->setDateOfBirthAt(\DateTimeImmutable::createFromMutable($faker->dateTimeInInterval("-30 years", "+10 years")));
+            $user->setBiography($faker->text());
             
-        //     for($j = 0; $j < rand(2, 4); $j++){
-        //         $post = new Publication();
-        //         $post->setContent($faker->paragraph(rand(3, 10)));
-        //         $post->setCreatedAt(\DateTimeImmutable::createFromMutable($faker->dateTimeBetween('-1 week', '-1day')));
-        //         $post->setUser($user);
-        //         $manager->persist($post);
+            for($j = 0; $j < rand(2, 4); $j++){
+                $post = new Publication();
+                $post->setContent($faker->paragraph(rand(3, 10)));
+                $post->setCreatedAt(\DateTimeImmutable::createFromMutable($faker->dateTimeBetween('-1 week', '-1day')));
+                $post->setUser($user);
+                $manager->persist($post);
 
-        //         for($k = 0; $k < rand(2,4); $k++){
-        //             $comment = new Comment();
-        //             $comment->setContent($faker->text());
-        //             $comment->setCreatedAt(\DateTimeImmutable::createFromMutable($faker->dateTimeBetween('$post->getCreatedAt', 'now')));
-        //             $comment->setPublication($post);
-        //             $comment->setUser($user);
-        //             $manager->persist($comment);
-        //         }
-        //     }
-        //     $manager->persist($user);
-        // }
-        // $manager->flush();
+                for($k = 0; $k < rand(2,4); $k++){
+                    $comment = new Comment();
+                    $comment->setContent($faker->text());
+                    $comment->setCreatedAt(\DateTimeImmutable::createFromMutable($faker->dateTimeBetween('$post->getCreatedAt', 'now')));
+                    $comment->setPublication($post);
+                    $comment->setUser($user);
+                    $manager->persist($comment);
+                }
+            }
+            $manager->persist($user);
+        }
+        $manager->flush();
     }
 }
