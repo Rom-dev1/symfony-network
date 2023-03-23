274da7accefddec59bda56df34e1b1dbb6f118ce
Romain Trochu <romain.trochu@gmail.com>
2023-03-23 20:27:43 +0100

    fix home controller, add if connected to template base

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,15 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(PublicationRepository $pub, UserRepository $repository): Response
+    #[Route('/', name: 'app_home')]
+    public function index(PublicationRepository $pub): Response
     {   
-        $user = $repository->findAll();
         $publi = $pub->findAll();
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'users' => $user,
             'publications' => $publi
         ]);
     }
