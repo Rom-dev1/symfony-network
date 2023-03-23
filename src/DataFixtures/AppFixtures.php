1c76ea723789bca23b50ed65d2844ed8f628e7bb
Romain Trochu <romain.trochu@gmail.com>
2023-03-23 13:21:53 +0100

    fixtures comment, init home ctr

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Comment;
 use App\Entity\Publication;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -28,7 +29,7 @@ class AppFixtures extends Fixture
         for($i = 0; $i <5; $i++){
             $user = new User();
             $user->setEmail($faker->word().'@mail.com');
-            $user->setPassword($faker->word()); //$this->hasher->hashPassword($user, $faker->word())
+            $user->setPassword($faker->word()); //@todo rmettre le hasher $this->hasher->hashPassword($user, $faker->word())
             $user->setRoles(['ROLE_USER']);
             $user->setFirstname($faker->firstName($gender[array_rand($gender)]));
             $user->setUsername($faker->lastName());
@@ -36,19 +37,24 @@ class AppFixtures extends Fixture
             $user->setDateOfBirthAt(\DateTimeImmutable::createFromMutable($faker->dateTimeInInterval("-30 years", "+10 years")));
             $user->setBiography($faker->text());
             
-
-            for($j = 0; $j <2; $j++){
+            for($j = 0; $j < rand(2, 4); $j++){
                 $post = new Publication();
                 $post->setContent($faker->paragraph(rand(1, 3)));
-                $post->setCreatedAt(\DateTimeImmutable::createFromMutable($faker->dateTimeBetween('-1 week', 'now')));
+                $post->setCreatedAt(\DateTimeImmutable::createFromMutable($faker->dateTimeBetween('-1 week', '-1day')));
                 $post->setUser($user);
                 $manager->persist($post);
+
+                for($k = 0; $k < rand(2,4); $k++){
+                    $comment = new Comment();
+                    $comment->setContent($faker->text());
+                    $comment->setCreatedAt(\DateTimeImmutable::createFromMutable($faker->dateTimeBetween('$post->getCreatedAt', 'now')));
+                    $comment->setPublication($post);
+                    $comment->setUser($user);
+                    $manager->persist($comment);
+                }
             }
-            
             $manager->persist($user);
         }
-
-        
         $manager->flush();
     }
 }
