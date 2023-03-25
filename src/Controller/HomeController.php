d22362f986f9fdd16a8bfa3babe3d9b1d1b44d12
Romain Trochu <romain.trochu@gmail.com>
2023-03-25 15:26:01 +0100

    make section member

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,7 @@ use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class HomeController extends AbstractController
 {
@@ -18,6 +19,17 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'publications' => $publi
+        ]);
+    }
+    
+    #[Route('/member', name:'app_member')]
+    #[IsGranted('ROLE_USER')]
+    public function showMember(UserRepository $members) : Response
+    {
+        $members = $members->findAll();
+
+        return $this->render('home/member.html.twig', [
+            'members' => $members
         ]);
     }
 }
